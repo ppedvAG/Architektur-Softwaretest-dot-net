b157c5fbded7e918066523a71b1ca6756368a5cd
André <andrer@ppedv.de>
2020-03-09 12:06:04 +0100

    Threadsafe Singelton

---
+++ HalloSingelton/HalloSingelton/Logger.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HalloSingelton
+{
+    public class Logger
+    {
+        private static Logger instance = null;
+        private static object lockObject = new object();
+
+        public static Logger Instance
+        {
+            get
+            {
+                lock (lockObject)
+                {
+                    if (instance == null)
+                        instance = new Logger();
+                }
+                return instance;
+            }
+        }
+
+        private Logger()
+        {
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("CTOR");
+            Console.ResetColor();
+        }
+
+        public void Log(string txt)
+        {
+            lock (lockObject)
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine($"[{DateTime.Now:d} {DateTime.Now:T}] {txt}");
+                Console.ResetColor();
+            }
+        }
+    }
+}
---
+++ HalloSingelton/HalloSingelton/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace HalloSingelton
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Singelton!");
+
+            for (int i = 0; i < 10; i++)
+            {
+                Task.Run(() => Logger.Instance.Log($"TEST {i}"));
+            }
+
+            //Logger.Instance.Log("TEST 1");
+            //Logger.Instance.Log("TEST 2");
+            //Logger.Instance.Log("TEST 3");
+
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+    }
+}
