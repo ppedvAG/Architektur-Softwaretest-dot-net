05c03318228d60a2aa4507718689cfb27e016960
André <andrer@ppedv.de>
2020-03-09 15:01:34 +0100

    Builder pattern sample

---
+++ HalloBuilder/HalloBuilder/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace HalloBuilder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello Builder!");
+
+            var schrank1 = new Schrank.SchrankBuilder()
+                              .SetAnzahlTüren(5)
+                              .SetAnzahlBöden(12)
+                              .Create();
+
+
+            var schrank2 = new Schrank.SchrankBuilder()
+                              .SetAnzahlTüren(4)
+                              .SetAnzahlBöden(4)
+                              .Pull().Pull().Update()
+                              .SetOberfläche(Oberfläche.Lackiert)
+                              .SetFarbe("red")
+                              .Create();
+
+            var schrank3 = new Schrank.SchrankBuilder()
+                  .SetAnzahlTüren(4)
+                  .SetOberfläche(Oberfläche.Gewachst)
+                  .SetFarbe("red")
+                  .Create();
+        }
+    }
+    public enum Oberfläche
+    {
+        Natur,
+        Lackiert,
+        Gewachst
+    }
+}
---
+++ HalloBuilder/HalloBuilder/Schrank.cs
@@ -0,0 +1,75 @@
+﻿using System;
+
+namespace HalloBuilder
+{
+    public class Schrank
+    {
+        public int AnzahlTüren { get; private set; }
+        public int AnzahlBöden { get; private set; }
+        public Oberfläche Oberfläche { get; private set; }
+        public string Farbe { get; private set; }
+        public bool Kleiderstange { get; private set; }
+
+        private Schrank() { }
+
+        public class SchrankBuilder
+        {
+            private Schrank schrank = new Schrank();
+
+            public Schrank Create() => schrank;
+
+            public SchrankBuilder SetAnzahlTüren(int türen)
+            {
+                if (türen >= 2 && türen <= 7)
+                {
+                    schrank.AnzahlTüren = türen;
+                    return this;
+                }
+                throw new ArgumentException("Es sind nur 2-7 Türen erlaubt");
+            }
+
+            public SchrankBuilder SetAnzahlBöden(int böden)
+            {
+                if (böden >= 0 && böden <= 12)
+                {
+                    schrank.AnzahlBöden = böden;
+                    return this;
+                }
+                throw new ArgumentException("Es sind nur 0-12 Böden erlaubt");
+            }
+
+            public SchrankBuilder SetFarbe(string farbe)
+            {
+                if (schrank.Oberfläche != Oberfläche.Lackiert)
+                    throw new ArgumentException("Nut Lackierte Schränke können eine Farbe haben");
+
+                if (farbe == "pink")
+                    throw new ArgumentException("igitt!!!");
+
+                schrank.Farbe = farbe;
+                return this;
+            }
+
+            public SchrankBuilder Update()
+            {
+                Pull();
+                return this;
+            }
+            public SchrankBuilder Pull()
+            {
+                //todo
+                return this;
+            }
+
+            public SchrankBuilder SetOberfläche(Oberfläche ober)
+            {
+                schrank.Oberfläche = ober;
+
+                if (ober == Oberfläche.Gewachst || ober == Oberfläche.Natur)
+                    schrank.Farbe = "";
+
+                return this;
+            }
+        }
+    }
+}
