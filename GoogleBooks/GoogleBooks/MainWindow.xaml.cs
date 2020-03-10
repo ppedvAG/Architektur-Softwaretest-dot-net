513951ec651bc06057db702fad8737f4d2dade3b
André <andrer@ppedv.de>
2020-03-10 11:11:13 +0100

    REST WPF GoogleBooks Async TPL

---
+++ GoogleBooks/GoogleBooks/MainWindow.xaml.cs
@@ -0,0 +1,84 @@
+﻿using Newtonsoft.Json;
+using System.IO;
+using System.Linq;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+
+namespace GoogleBooks
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Laden(object sender, RoutedEventArgs e)
+        {
+            var url = $"https://www.googleapis.com/books/v1/volumes?q={suchTb.Text}";
+
+            var web = new HttpClient();
+            var json = await web.GetStringAsync(url);
+
+            BooksResult result = JsonConvert.DeserializeObject<BooksResult>(json);
+
+            myGrid.ItemsSource = result.items.Select(x => x.volumeInfo).ToList();
+
+        }
+
+        private async void HalloAsync(object sender, RoutedEventArgs e)
+        {
+            //stop UI 
+            //https://stackoverflow.com/a/3383010
+            var d = Dispatcher.DisableProcessing();
+
+
+            ((Button)sender).IsEnabled = !true;
+
+            for (int i = 0; i < 100; i++)
+            {
+                pb1.Value = i;
+                await Task.Delay(100);
+                var sr = new StreamReader(@"E:\eula.txt");
+                var txt = await sr.ReadToEndAsync();
+            }
+            ((Button)sender).IsEnabled = !!true;
+
+            d.Dispose();
+        }
+
+        private void HalloTask(object sender, RoutedEventArgs e)
+        {
+            //Dispatcher = doof aber geht: muss auf UI warten
+            //Task.Run(() =>
+            //{
+            //    for (int i = 0; i < 100; i++)
+            //    {
+            //        pb1.Dispatcher.Invoke(() => pb1.Value = i);
+            //        Thread.Sleep(100);
+            //    }
+            //});
+
+
+            //cool: ab .NET 4
+            ((Button)sender).IsEnabled = !true;
+            var ts = TaskScheduler.FromCurrentSynchronizationContext();
+            Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Task.Factory.StartNew(() => pb1.Value = i, CancellationToken.None, TaskCreationOptions.None, ts);
+                    Thread.Sleep(50);
+                }
+                Task.Factory.StartNew(() => ((Button)sender).IsEnabled = true, CancellationToken.None, TaskCreationOptions.None, ts);
+            });
+
+        }
+    }
+}
---
+++ HalloDecorator/HalloDecorator/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace HalloDecorator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            var brot = new Käse(new Brot());
+            Console.WriteLine($"{brot.Name} {brot.Preis:c}");
+
+            var pizza = new Salami(new Käse(new Käse(new Pizza())));
+            Console.WriteLine($"{pizza.Name} {pizza.Preis:c}");
+
+            Console.WriteLine("Ende");
+            Console.ReadKey();
+        }
+    }
+
+    public interface ICompo
+    {
+        public string Name { get; }
+        public decimal Preis { get; }
+    }
+
+    public class Pizza : ICompo
+    {
+        public string Name => "Pizza";
+
+        public decimal Preis => 5.8m;
+    }
+
+    public class Brot : ICompo
+    {
+        public string Name => "Brot";
+
+        public decimal Preis => 2.3m;
+    }
+
+    public abstract class Deco : ICompo
+    {
+        protected readonly ICompo parent;
+
+        public Deco(ICompo compo)
+        {
+            parent = compo;
+        }
+
+        public abstract string Name { get; }
+
+        public abstract decimal Preis { get; }
+
+    }
+
+    public class Käse : Deco
+    {
+        public Käse(ICompo compo) : base(compo) { }
+
+        public override string Name => $"{parent.Name} Käse";
+
+        public override decimal Preis => parent.Preis + 1.2m;
+    }
+
+    public class Salami : Deco
+    {
+        public Salami(ICompo compo) : base(compo) { }
+
+        public override string Name => $"{parent.Name} Salmi";
+
+        public override decimal Preis => parent.Preis + 2.33m;
+    }
+}
