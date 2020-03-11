63f0926db664fc62e27184b9cb920de0121a0dbc
André <andrer@ppedv.de>
2020-03-11 14:32:42 +0100

    jetzt mit Logic und Repository und Consolen UI

---
+++ ppedv.Pandemia/ppedv.Pandemia.Data.EF/EfRepository.cs
@@ -0,0 +1,46 @@
+﻿using ppedv.Pandemia.Model;
+using ppedv.Pandemia.Model.Contracts;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ppedv.Pandemia.Data.EF
+{
+    public class EfRepository : IRepository
+    {
+        EfContext context = new EfContext();
+
+        public void Add<T>(T entity) where T : Entity
+        {
+            context.Set<T>().Add(entity);
+        }
+
+        public void Delete<T>(T entity) where T : Entity
+        {
+            context.Set<T>().Remove(entity);
+        }
+
+        public IEnumerable<T> GetAll<T>() where T : Entity
+        {
+            return context.Set<T>().ToList();
+        }
+
+        public T GetById<T>(int id) where T : Entity
+        {
+            return context.Set<T>().Find(id);
+        }
+
+        public void SaveAll()
+        {
+            context.SaveChanges();
+        }
+
+        //Web / Serivces (Disconnected)
+        //nicht bei WPF/Winforms (Desktop)
+        public void Update<T>(T entity) where T : Entity
+        {
+            var loaded = GetById<T>(entity.Id);
+            if (loaded != null)
+                context.Entry(loaded).CurrentValues.SetValues(entity);
+        }
+    }
+}
---
+++ ppedv.Pandemia/ppedv.Pandemia.UI.DevConsole/Program.cs
@@ -0,0 +1,25 @@
+﻿using ppedv.Pandemia.Logic;
+using ppedv.Pandemia.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ppedv.Pandemia.UI.DevConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var core = new Core();
+
+            foreach (var inf in core.Repository.GetAll<Infektion>())
+            {
+                Console.WriteLine($"{inf.Person} ({inf.GebDatum:d}) aus {inf.Wohnort.Name},{inf.Wohnort.Land.Name}");
+                inf.Viren.ToList().ForEach(x => Console.WriteLine($"\t {x.Name}"));
+            }
+
+        }
+    }
+}
