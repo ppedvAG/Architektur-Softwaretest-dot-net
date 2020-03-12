9686b0e1967173e69217971a742df5dcb90abf45
André <andrer@ppedv.de>
2020-03-12 15:25:29 +0100

    Fody

--- ppedv.Pandemia/ppedv.Pandemia.Model/Entity.cs
+++ ppedv.Pandemia/ppedv.Pandemia.Model/Entity.cs
@@ -1,12 +1,15 @@
 ﻿using System;
+using System.ComponentModel;
 
 namespace ppedv.Pandemia.Model
 {
-    public abstract class Entity
+    public abstract class Entity : INotifyPropertyChanged
     {
         public Guid Id { get; set; } = new Guid();
         public DateTime Created { get; set; } = DateTime.Now;
         public DateTime Modified { get; set; } = DateTime.Now;
+
+        public event PropertyChangedEventHandler PropertyChanged;
     }
 
 }
