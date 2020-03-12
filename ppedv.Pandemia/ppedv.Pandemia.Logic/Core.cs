b468f1a60c177baddb961313b36df11941404d1e
André <andrer@ppedv.de>
2020-03-12 11:33:24 +0100

    Core als Singelton

--- ppedv.Pandemia/ppedv.Pandemia.Logic/Core.cs
+++ ppedv.Pandemia/ppedv.Pandemia.Logic/Core.cs
@@ -2,18 +2,34 @@
 using ppedv.Pandemia.Model.Contracts;
 using System;
 using System.Linq;
+using System.Runtime.CompilerServices;
+
+[assembly: InternalsVisibleTo("ppedv.Pandemia.Logic.Tests")]
 
 namespace ppedv.Pandemia.Logic
 {
     public class Core
     {
 
-
+        private static Core core;
+        private static object sync = new object();
+        public static Core Instance
+        {
+            get
+            {
+                lock (sync)
+                {
+                    if (core == null)
+                        core = new Core(new Data.XML.XmlRepository(@"C:\Users\ar2\source\repos\ppedvAG\Architekt_NBG_09032020\ppedv.Pandemia\ppedv.Pandemia.Data.XML.Test\bin\Debug\AutoFix.xml"));
+                }
+                return core;
+            }
+        }
 
 
         public IRepository Repository { get; private set; }
 
-        public Core(IRepository repo)
+        internal Core(IRepository repo)
         {
             Repository = repo;
             LandService = new LandServices(this);
